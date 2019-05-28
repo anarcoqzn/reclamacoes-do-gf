cb72c9ab9558b391265d7431ffec71af65e0443e
anarcoqr <anarcoqr@gmail.com>
2019-05-28 17:32:42 -0300

    usando a moda e tratando a tabela de reclamacoes

--- code/analise_exploratoria.R
+++ code/analise_exploratoria.R
@@ -1,7 +1,3 @@
-install.packages("tidyverse")
-install.packages("ggplot2")
-install.packages("magrittr")
-
 library(tidyverse)
 library(ggplot2)
 library(magrittr)
@@ -10,8 +6,19 @@ avaliacoes <- read.csv(file = "data/3-avaliacao-humana/avaliacoes20190515.csv")
 
 avaliacoes <- avaliacoes %>% 
     select(avaliador = `Matricula`, 
-           id = `ID da reclamação`, 
-           insatisfacao = `Grau de insatisfação`)
+           id = `ID.da.reclamação`, 
+           insatisfacao = `Grau.de.insatisfação`)
 
 avaliacoes %>% 
     filter((id %in% 1:5 ))
+
+moda <- function(v) {
+    uniqv <- unique(v)
+    uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+avaliacao <- avaliacoes %>% select(id, insatisfacao) %>% 
+    group_by(id) %>% 
+    summarise(grau_insatisfacao = moda(insatisfacao), 
+              avaliadores = n(),
+              range.avaliacoes = (max(insatisfacao) - min(insatisfacao)))
